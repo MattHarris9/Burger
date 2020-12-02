19b3fc1d5c281adfe0e2c10e87b9696dea66df16
Matt Harris <711mattman@gmail.com>
2020-12-02 17:43:25 -0500

    Added the burger data into the seed sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ use burgers_db;
 create table burgers (
 id int not null auto_increment,
 burger_name varchar(60) not null,
-devoured boolean not null default 0,
+devoured boolean not null default 1,
 primary key(id)
 )
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured) values ("The sunshine burger", 1);
+insert into burgers (burger_name, devoured) values ("The sloppy ned", 1);
+insert into burgers (burger_name, devoured) values ("MIdknight rider", 0);
+insert into burgers (burger_name, devoured) values ("The good, bad, and ugly burger", 1);
+insert into burgers (burger_name, devoured) values ("Misery loves company", 1);
+insert into burgers (burger_name, devoured) values ("What happens in vegas", 1);
+insert into burgers (burger_name, devoured) values ("Double cheese burger", 0);
