113afb7147df82e99bb346608543c85fa47f3f43
Matt Harris <711mattman@gmail.com>
2020-12-04 13:23:07 -0500

    Made sure the connection to the server is working just fine

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,37 +3,37 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/cats", function(req, res) {
-  cat.all(function(data) {
-    res.json({ cats: data });
+router.get("/burgers", function(req, res) {
+  burger.all(function(data) {
+    res.json({ burgers: data });
   });
 });
 
-router.post("/cats", function(req, res) {
+router.post("/burgers", function(req, res) {
   cat.create([
-    "name", "sleepy"
+    "burger_name"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/cats/:id", function(req, res) {
+router.put("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.update({
+    devoured: true 
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -44,10 +44,10 @@ router.put("/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/cats/:id", function(req, res) {
+router.delete("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
